d54e01d3ad84d3cfdbfa532eddbb53c83b7edaca
wardavison <wardavison@gmail.com>
2014-09-28 22:52:58 +0100

    Simple examplestack that outputs a single VPC resource

---
+++ README.md
@@ -0,0 +1,9 @@
+CloudformationExample
+=====================
+
+Example making use of CloudformationBase to generate a Cloudformation template
+
+Author
+======
+
+Andrew Davison
---
+++ examplestack.py
@@ -0,0 +1,28 @@
+"""  Example stack
+"""
+from CloudformationBase.BaseVPC import BaseVPC
+from CloudformationBase.stack import Stack
+
+
+class ExampleStack(Stack):
+    """ Example Stack class
+    """
+
+    def __init__(self):
+        super(ExampleStack, self).__init__('Example', 'Dev')
+
+    def populate_template(self):
+        examplevpc = BaseVPC(self.projectname, self.environmentname)
+        self.template.add_resource(examplevpc.res_vpc)
+
+    def output_template(self):
+        super(ExampleStack, self).output_template()
+
+
+def main():
+    thestack = ExampleStack()
+    thestack.populate_template()
+    thestack.output_template()
+
+if __name__ == "__main__":
+    main()
