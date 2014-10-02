57456c11c357b885e19a4952aa12305272b17797
wardavison <wardavison@gmail.com>
2014-10-02 23:42:11 +0100

    Updated Example to match new CloudformationBase structure. Added example of overriding the CloudformationBase.

--- examplestack.py
+++ examplestack.py
@@ -2,6 +2,8 @@
 """
 from CloudformationBase.BaseVPC import BaseVPC
 from CloudformationBase.stack import Stack
+from examplevpc import ExampleVPC
+from troposphere import Ref
 
 
 class ExampleStack(Stack):
@@ -9,15 +11,32 @@ class ExampleStack(Stack):
     """
 
     def __init__(self):
+        # Initialise the Stack with a Project name and Environment Name
         super(ExampleStack, self).__init__('Example', 'Dev')
 
-    def populate_template(self):
-        examplevpc = BaseVPC(self.projectname, self.environmentname)
-        for resource in examplevpc.resources:
-            self.template.add_resource(resource)
+        # Create a parameter for taking the VPC CIDR block
+        self.par_vpccidrblock = \
+            BaseVPC.par_vpccidrblock('BaseVPCCIDR')
+        # Do the same from the ExampleVPC class
+        self.par_examplevpccidrblock = \
+            ExampleVPC.par_vpccidrblock('ExampleVPCCIDR')
 
-    def output_template(self):
-        super(ExampleStack, self).output_template()
+        # Create a VPC using the BaseVPC class
+        self.res_vpc = \
+            BaseVPC.res_vpc('BaseVPC', Ref(self.par_vpccidrblock), True, True,
+                            'default', self.projectname, self.environmentname)
+        # Create a VPC using the ExampleVPC class (Which overrides BaseVPC)
+        self.res_overriden_vpc = \
+            ExampleVPC.res_vpc('ExampleVPC', Ref(self.par_examplevpccidrblock),
+                               True, True, 'default', self.projectname,
+                               self.environmentname)
+
+    def populate_template(self):
+        self.parameters.append(self.par_vpccidrblock)
+        self.parameters.append(self.par_examplevpccidrblock)
+
+        self.resources.append(self.res_vpc)
+        self.resources.append(self.res_overriden_vpc)
 
 
 def main():
---
+++ examplevpc.py
@@ -0,0 +1,37 @@
+""" Example VPC with overrides
+"""
+from troposphere import Join
+from troposphere.ec2 import VPC, Tag
+from CloudformationBase import overrides
+from CloudformationBase.BaseVPC import BaseVPC
+
+
+class ExampleVPC(BaseVPC):
+
+    def __init__(self):
+        super(ExampleVPC, self).__init__()
+
+    @staticmethod
+    def res_vpc(title, cidrblock, dnssupport, dnshostnames,
+                instancetenancy, projectname, environmentname):
+        """
+        :param title: Title for the JSON VPC resource (string)
+        :param cidrblock: CIDR block for the VPC e.g. 10.0.0.0/16 (string)
+        :param dnssupport: The VPC has DNS support (boolean)
+        :param dnshostnames: Enable DNS hostnames (boolean)
+        :param instancetenancy: default or dedicated instance tenancy (string)
+        :param projectname: Name of project (used to form name) (string)
+        :param environmentname: Name of environment (used to form name) (string)
+        :return: Troposphere VPC resource object
+        """
+        return VPC(
+            title,
+            CidrBlock=cidrblock,
+            EnableDnsSupport=dnssupport,
+            EnableDnsHostnames=dnshostnames,
+            InstanceTenancy=instancetenancy,
+            Tags=[Tag('Name', Join('', [projectname,
+                                        '-',
+                                        environmentname,
+                                        '-overridenVPC']))]
+        )
